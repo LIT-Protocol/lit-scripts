43244134800a3af2ec8f58da93fe07635c2fe831
Ansonhkg <ansonox@gmail.com>
2023-09-12 23:20:10 +0100

    docs: update

--- setup-sdk-tests.sh
+++ setup-sdk-tests.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+headerLog() {
+  echo
+  echo "===== $1 ====="
+}
+
 curl -fsSL https://bun.sh/install | bash
 
+headerLog "🔍 Cloning the repository"
 git clone https://github.com/LIT-Protocol/test-sdk-for-nodes-ppl .
+
+headerLog "🚀 Starting the installation process"
+bun install
+

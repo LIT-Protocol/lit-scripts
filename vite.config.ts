84a0a55d2c2dd95e377e0f107a4fee1a3f9fc035
Anson <ansonox@gmail.com>
2024-07-17 22:11:05 +0100

    revert changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0",
-    port: 4173, 
-  },
   define: {
     global: "window",
   },
@@ -16,4 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})

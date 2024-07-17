66ba31b93bc863be2260d325edbbe27ad4239759
Anson <ansonox@gmail.com>
2024-07-17 22:18:25 +0100

    fix: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills()
+  ],
   server: {
     host: '0.0.0.0', // Exposes the server to all network interfaces
     port: 4173, // Specify the port you want to use
@@ -14,6 +19,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      buffer: 'buffer'
     },
   },
 })

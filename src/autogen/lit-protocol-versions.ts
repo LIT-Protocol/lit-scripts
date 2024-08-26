136bd82f0336b45cadd6ffdf71e44c252a79cc60
Anson <ansonox@gmail.com>
2024-08-26 15:23:02 +0100

    chore: update packages and components

--- src/autogen/lit-protocol-versions.ts
+++ src/autogen/lit-protocol-versions.ts
@@ -1,6 +1,6 @@
 export const LIT_PACKAGES = {
-  "@lit-protocol/constants": "^6.2.3-alpha.1",
-  "@lit-protocol/contracts": "^0.0.37",
-  "@lit-protocol/lit-auth-client": "^6.2.3-alpha.1",
-  "@lit-protocol/lit-node-client": "^6.2.3-alpha.1"
+  "@lit-protocol/constants": "6.4.10-alpha.0",
+  "@lit-protocol/contracts": "^0.0.63",
+  "@lit-protocol/lit-auth-client": "6.4.10-alpha.0",
+  "@lit-protocol/lit-node-client": "6.4.10-alpha.0"
 } as const;
